76d9008054d52a1fb928fc0c73705c4ad8c05a80
hackingboy2020 <79807496+hackingboy2020@users.noreply.github.com>
2021-04-10 21:31:05 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,11 @@ echo -e "$rojo                :        :                                     "
                                                                
                                                                
 pkg install python -y
+pkg install python2 -y
 pkg update -y
 
 sudo apt-get install python -y
+sudo apt-get install python2 -y
 sudo apt update -y
 
 
